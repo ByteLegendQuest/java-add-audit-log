6151ec6bc991977b8023b7185ae74fb8c1d8c5c7
SpaceTimeAdministrator <106767177+SpaceTimeAdministrator@users.noreply.github.com>
2022-06-19 10:54:34 +0000

    Update at 2022-06-19T10:54:34.553788Z

--- src/main/java/com/bytelegend/noencapsulation/Utils.java
+++ src/main/java/com/bytelegend/noencapsulation/Utils.java
@@ -9,30 +9,28 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Utils {
-    private static Person person;
-
     public static List<Person> filterPeopleWithSalaryMoreThan1(List<Person> people, int salary) {
-        return people.stream().filter(p -> {Logger.logAccessToSalaryOfPerson(p.name);
+        return people.stream().filter(p -> {
+            Logger.logAccessToSalaryOfPerson(p.name);
             return p.salary > salary;
         }).collect(Collectors.toList());
     }
-
     public static List<Person> filterPeopleWithSalaryMoreThan2(List<Person> people, int salary) {
         List<Person> list = new ArrayList<>();
         for (Person p : people) {
-            Logger.logAccessToSalaryOfPerson(p.name);
             if (p.salary > salary) {
+                Logger.logAccessToSalaryOfPerson(p.name);
                 list.add(p);
             }
         }
         return list;
     }
-
     public static Map<String, Integer> getNameToSalaryMap1(List<Person> people) {
-        return people.stream().collect(Collectors.toMap(p -> p.name, p -> {Logger.logAccessToSalaryOfPerson(p.name);
-            return p.salary;}));
+        return people.stream().collect(Collectors.toMap(p -> p.name, p -> {
+            Logger.logAccessToSalaryOfPerson(p.name);
+            return p.salary;
+        }));
     }
-
     public static Map<String, Integer> getNameToSalaryMap2(List<Person> people) {
         Map<String, Integer> map = new HashMap<>();
         for (Person p : people) {
