e15a7b45854490e97e920e30ff5b82a669a72a1b
pipiloveslife <Pain_Zzz@163.com>
2022-10-12 05:35:48 +0000

    Update at 2022-10-12T05:35:48.740663Z

--- src/main/java/com/bytelegend/encapsulation/Person.java
+++ src/main/java/com/bytelegend/encapsulation/Person.java
@@ -29,7 +29,7 @@ public class Person {
     }
 
     public int getSalary() {
-        Logger.logAccessToSalaryOfPerson(person.name);
+        Logger.logAccessToSalaryOfPerson(this.name);
         return salary;
     }
 }
