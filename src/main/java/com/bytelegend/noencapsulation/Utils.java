fc35bb485c7bee2dc01b5c0b4d80465b96316b22
YuanHao97 <y1997h@126.com>
2022-10-15 13:04:06 +0000

    Update at 2022-10-15T13:04:06.752594Z

--- src/main/java/com/bytelegend/noencapsulation/Utils.java
+++ src/main/java/com/bytelegend/noencapsulation/Utils.java
@@ -1,5 +1,7 @@
 package com.bytelegend.noencapsulation;
 
+import com.bytelegend.Logger;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -14,6 +16,7 @@ public class Utils {
     public static List<Person> filterPeopleWithSalaryMoreThan2(List<Person> people, int salary) {
         List<Person> list = new ArrayList<>();
         for (Person p : people) {
+            Logger.logAccessToSalaryOfPerson(p.name);
             if (p.salary > salary) {
                 list.add(p);
             }
@@ -28,6 +31,7 @@ public class Utils {
     public static Map<String, Integer> getNameToSalaryMap2(List<Person> people) {
         Map<String, Integer> map = new HashMap<>();
         for (Person p : people) {
+            Logger.logAccessToSalaryOfPerson(p.name);
             map.put(p.name, p.salary);
         }
         return map;
