5683e25510e194846371b7c3dd5eb577c31bab38
Tomcat-Guo <zhiqiang.guo@shopee.com>
2022-11-28 07:35:47 +0000

    Update at 2022-11-28T07:35:47.301185Z

--- src/main/java/com/bytelegend/noencapsulation/Utils.java
+++ src/main/java/com/bytelegend/noencapsulation/Utils.java
@@ -8,12 +8,20 @@ import java.util.stream.Collectors;
 
 public class Utils {
     public static List<Person> filterPeopleWithSalaryMoreThan1(List<Person> people, int salary) {
-        return people.stream().filter(p -> p.salary > salary).collect(Collectors.toList());
+        List<Person> list = new ArrayList<>();
+        for (Person p : people) {
+            Logger.logAccessToSalaryOfPerson(p.name)
+            if (p.salary > salary) {
+                list.add(p);
+            }
+        }
+        return list;
     }
 
     public static List<Person> filterPeopleWithSalaryMoreThan2(List<Person> people, int salary) {
         List<Person> list = new ArrayList<>();
         for (Person p : people) {
+            Logger.logAccessToSalaryOfPerson(p.name)
             if (p.salary > salary) {
                 list.add(p);
             }
@@ -22,12 +30,18 @@ public class Utils {
     }
 
     public static Map<String, Integer> getNameToSalaryMap1(List<Person> people) {
-        return people.stream().collect(Collectors.toMap(p -> p.name, p -> p.salary));
+        Map<String, Integer> map = new HashMap<>();
+        for (Person p : people) {
+            Logger.logAccessToSalaryOfPerson(p.name)
+            map.put(p.name, p.salary);
+        }
+        return map;
     }
 
     public static Map<String, Integer> getNameToSalaryMap2(List<Person> people) {
         Map<String, Integer> map = new HashMap<>();
         for (Person p : people) {
+            Logger.logAccessToSalaryOfPerson(p.name)
             map.put(p.name, p.salary);
         }
         return map;
